6a1836fefa8411e74f0ade46eb1ab58b094cc2b3
unknown <mzyranov26@gmail.com>
2023-12-06 07:57:35 +0400

    Updated Calculator class

--- Code/Src/class.cpp
+++ Code/Src/class.cpp
@@ -1,22 +1,53 @@
 #include <iostream>
 #include <cstdint>
+#include <cmath>
+#include <initializer_list>
 
-template <typename T>
 class Calculator
 {
 public:
   // ADD INITIALIZED LIST (so that we can add as many data as we want)
-  Calculator() = default;
-  Calculator(T a, T b)
-      : m_A(a), m_B(b) {}
-  
-private:
-  T m_A;
-  T m_B;
+  template <typename T>
+  inline static T Add(const T &a, const T &b) { return a + b; }
+  template <typename T>
+  inline static T Add(const std::initializer_list<T> &data)
+  {
+    T result = (T)0;
+    for (const auto &d : data)
+      result += d;
+    return result;
+  }
+  template <typename T>
+  inline static T Multiply(const T &a, const T &b) { return a * b; }
+  template <typename T>
+  inline static T Multiply(const std::initializer_list<T> &data)
+  {
+    T result = (T)1;
+    for (const auto &d : data)
+      result *= d;
+    return result;
+  }
+  template <typename T>
+  inline static T Divide(const T &a, const T &b) { return a / b; }
+  template <typename T>
+  inline static T Substruct(const T &a, const T &b) { return a - b; }
+  template <typename T>
+  inline static T Substruct(const std::initializer_list<T> &data)
+  {
+    T result = (T)0;
+    for (const auto &d : data)
+      result -= d;
+    return result;
+  }
+  template <typename T>
+  inline static T Pow(const T &a, const T &b) { return pow(a, b); }
+
 };
 
 int main()
 {
+  std::cout << Calculator::Substruct({3.3f, 4.4f}) << '\n';
+  std::cout << Calculator::Pow(3.3f, 4.4f);
 
   return 0;
 }
