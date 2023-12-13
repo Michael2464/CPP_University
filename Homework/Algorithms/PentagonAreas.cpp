694d107ee755b40d152c10cc8b6097e26a37bf4f
unknown <mzyranov26@gmail.com>
2023-12-13 15:08:18 +0400

    Added new homework task

---
+++ Homework/Algorithms/PentagonAreas.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <array>
+
+typedef std::array<float, 4> coordinates;
+
+
+// *(1)  *(2)
+// 
+// *(3)  *(4)
+void EnterCoords(coordinates& coords){
+
+}
+
+int main(){
+  coordinates coords1;
+  coordinates coords2;
+
+
+
+  return 0;
+}
